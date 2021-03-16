047c19bffe41a34707a26646895c9ff538c5da10
daniyaniazi <farihadania@hotmail.com>
2021-03-16 18:51:22 +0500

    api UI for frontend

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
-
+from rest_framework.schemas import get_schema_view
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.documentation import include_docs_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,4 +18,10 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+
+    path("schema", get_schema_view(
+        title='Blog',
+        description="Simple Blog API",
+        version='1.0'), name="openapi-schema"),
+    path('docs/', include_docs_urls(title="BlogAPI"))
 ]
